8fcd6a2bbec83b9f9a03f7e22ab1e1102be23a76
Roly Daco <roly.damian@gmail.com>
2022-10-24 21:32:55 -0500

    Primer commit

---
+++ controllers/usuarios.js
@@ -0,0 +1,60 @@
+const { response, request } = require('express');
+
+
+const usuariosGet = (req, res = response) => {
+
+    //const query = req.query; Con esto se hace que (http://localhost:8080/api/usuarios?q=hola&nombre=roly&apikey=1234567890) se muestre ordenado.
+   const { q, nombre = 'No name', apikey, page = "1", limit } = req.query;
+    res.json({
+        msg: 'get API - controlador',
+        q,
+        nombre,
+        apikey,
+        page,
+        limit
+    });    
+}
+
+const usuariosPost = (req, res = response) => {
+
+    const { nombre, edad } = req.body;
+    //const body = req.body;
+   
+    res.json({
+        msg: 'post API - usuariosPost',
+        //body
+        nombre,
+        edad
+    });    
+}
+const usuariosPut = (req, res = response) => {
+
+    const { usuarioid } = req.params;
+   
+    res.json({
+        msg: 'put API - usuariosPut',
+        usuarioid
+    });    
+}
+const usuariosPatch = (req, res = response) => {
+   
+    res.json({
+        msg: 'patch API - usuariosPatch'
+    });    
+}
+const usuariosDelete = (req, res = response) => {
+   
+    res.json({
+        msg: 'delete API - usuariosDelete'
+    });    
+}
+
+
+module.exports = {
+    usuariosGet,
+    usuariosPost,
+    usuariosPut,
+    usuariosPatch,
+    usuariosDelete
+    
+}
